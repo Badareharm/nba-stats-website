fdebc59d20ed85e01e0af1d2ae9599c7737b8262
Muhammad Ahmad <machannar2007@gmail.com>
2025-04-09 10:14:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function fetchStats() {
-    const year = document.getElementById("year").value;
-    const mode = document.getElementById("mode").value;
-
-    document.getElementById("stats").innerHTML = `Fetching stats for ${year}, Mode: ${mode}...`;
-}
